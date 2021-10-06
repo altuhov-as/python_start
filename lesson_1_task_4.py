7f3fecf1a3d9728ae9821bb197a6003e53bbf4fb
altuhov_as <altuhov_as@mail.ru>
2021-10-06 23:05:24 +0300

    изменил условие в цикле и убрал печать лишних данных

--- lesson_1_task_4.py
+++ lesson_1_task_4.py
@@ -1,12 +1,11 @@
 num = int(input("Введите число: "))
 
 max_num = -1
-while num > 10:
+while num != 0:
     if max_num == 9:
         break
     d = num % 10
     num //= 10
-    print(d, num)
     if d > max_num:
         max_num = d
 print(max_num)
