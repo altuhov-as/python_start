c3977733eed1f479e76a3d2152798b515e4f7e60
altuhov-as <90609754+altuhov-as@users.noreply.github.com>
2021-10-12 22:33:48 +0300

    Добавлено мое решение

--- lesson_3_task_2.py
+++ lesson_3_task_2.py
@@ -3,6 +3,13 @@ def get_data(first_name: str = None, last_name: str = None, year: int = None, ci
     return f"{first_name} {last_name} ({year}), {city}, {phone}, {email}"
 
 
+#мой вариант решения
+#функция не отвечает параметрам "Функция должна принимать параметры как именованные аргументы"
+def get_data_2(**data):
+    for key, value in data.items():
+        print("{} ".format(value), end=" ")
+
+
 user_first_name = input("Имя: ")
 user_last_name = input("Фамилия: ")
 user_year = int(input("Год рождения: "))
@@ -12,3 +19,6 @@ user_phone = input("Номер телефона: ")
 
 print(get_data(first_name=user_first_name, last_name=user_last_name, year=user_year, city=user_city,
                email=user_email, phone=user_phone))
+
+get_data_2(first_name=user_first_name, last_name=user_last_name, year=user_year, city=user_city,
+               email=user_email, phone=user_phone)
